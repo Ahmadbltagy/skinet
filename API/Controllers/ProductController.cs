63292327788158beba8c5a90d7c6ab2e0a791881
Ahmad ElBeltagy <aelbeltagy@arpuplus.com>
2023-07-12 11:20:48 +0300

    Initial Commit

---
+++ API/Controllers/ProductController.cs
@@ -0,0 +1,32 @@
+﻿using Core.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly StoreContext _context;
+        public ProductController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetProducts()
+        {
+            var products = _context.Prodcuts.ToListAsync();
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProdcut(int id)
+        {
+            var product = _context.Prodcuts.Find(id);
+            return Ok(product);
+        }
+    }
+}
